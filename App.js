da5892d269ddcd12ac2f60b8f79c1698ce568f1e
yannesan <yanne.marzouk@epitech.eu>
2020-10-13 08:52:24 +0200

    Initialisation 2.0

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
-      <StatusBar style="auto" />
+
     </View>
   );
 }
